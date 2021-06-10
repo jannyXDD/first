e4d7466d6324e0614c62314e2349687867589fb3
jannyXDD <joaobit@hotmail.com>
2021-06-11 00:28:56 +0100

    a

---
+++ estduo tw/Ex18/codigo.js
@@ -0,0 +1,18 @@
+var imagens = ["img1.jpg","img2.jpg","img3.jpg"];
+var i = 0;
+var elemento = document.getElementById("slide");
+var idTemporizador = setInterval(imagemSeguinte,5000);
+
+/* chamar de 5s em 5s */
+function imagemSeguinte() {
+    i = (i + 1) % imagens.length;
+    elemento.src = "imagens/" + imagens[i];
+}
+
+function pararSlide() {
+    clearInterval(idTemporizador);
+}
+
+function seguinte() {
+    setTimeout(imagemSeguinte,500);
+}
